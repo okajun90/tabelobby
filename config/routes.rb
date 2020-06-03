cf20a6b1222444781886102a958dd9c1d8035eca
okajun90 <okajun1001@gmail.com>
2020-06-03 17:49:28 +0900

    controllerとmodelを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :stores
+  devise_for :users
+  resources :stores
+  resources :users
+  root to: 'tops#top'
+  get 'home/about', to: 'tops#about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
